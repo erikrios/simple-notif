6b2d000a1f88571c4479426b42436432ff5cc261
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-09 10:28:32 +0700

    Integrated the view binding

--- app/src/main/java/com/erikriosetiawan/simplenotif/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/simplenotif/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.erikriosetiawan.simplenotif
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.simplenotif.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
